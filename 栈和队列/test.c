352b9fa32dbf393ccc46213af4b1c82859b0af31
南孩 <wyt2538075419@163.com>
2022-08-28 15:13:45 +0800

    栈和队列

---
+++ 栈和队列/test.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "Stack.h"
+#include "Queue.h"
+void TestStack()
+{
+	ST st;
+	StackInit(&st);
+
+	StackPush(&st, 1);
+	StackPush(&st, 2);
+	StackPush(&st, 3);
+	StackPush(&st, 4);
+
+	while (!StackEmpty(&st))
+	{
+		printf("%d ", StackTop(&st));
+		StackPop(&st);
+	}
+	printf("\n");
+	printf("%d ", StackTop(&st));
+
+}
+
+void TestQueue()
+{
+	Queue q;
+	QueueInit(&q);
+
+	QueuePush(&q, 1);
+	QueuePush(&q, 2);
+	QueuePush(&q, 3);
+	QueuePush(&q, 4);
+	QueuePush(&q, 5);
+
+	while (!QueueEmpty(&q))
+	{
+		printf("%d ", QueueFront(&q));
+		QueuePop(&q);
+	}
+	printf("\n");
+}
+int main()
+{
+	//TestStack();
+	TestQueue();
+
+	return 0;
+}
