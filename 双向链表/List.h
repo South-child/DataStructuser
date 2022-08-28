da7c9e5b0b6c0436894e9181d1a380d874224a8c
南孩 <wyt2538075419@163.com>
2022-08-28 15:11:50 +0800

    双向链表

---
+++ 双向链表/List.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+
+typedef int LTDataType;
+
+typedef struct ListNode
+{
+	struct ListNode* next;
+	struct ListNode* prev;
+	LTDataType data;
+}LTNode;
+
+//初始化
+LTNode* ListInit();
+//销毁
+void ListDestory(LTNode* phead);
+//打印
+void ListPrint(LTNode* phead);
+
+//尾插
+void ListPushBack(LTNode* phead, LTDataType x);
+//尾删
+void ListPopBack(LTNode* phead);
+
+//头插
+void ListPushFront(LTNode* phead, LTDataType x);
+//头删
+void ListPopFront(LTNode* phead);
+
+//查找
+LTNode* ListFind(LTNode* phead, LTDataType x);
+//在pos位置之前插入
+void ListInsert(LTNode* pos, LTDataType x);
+//删除pos位置
+void ListErase(LTNode* pos);
+
+//判断链表是否为空
+bool ListEmpty(LTNode* phead);
