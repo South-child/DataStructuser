dff5681cab19e0c9ca3b14d286c317f5dd986dfa
South-child <2538075419@qq.com>
2022-08-29 22:12:16 +0800

    二叉树之堆

---
+++ 二叉树-堆/Heap.c
@@ -0,0 +1,122 @@
+#include "Heap.h"
+
+void HeapPrint(HP* php)
+{
+	for (int i = 0; i < php->size; ++i)
+	{
+		printf("%d ", php->a[i]);
+	}
+	printf("\n");
+}
+
+void HeapInit(HP* php)
+{
+	assert(php);
+	php->a = NULL;
+	php->size = php->capacity = 0;
+}
+
+void HeapDestroy(HP* php);
+
+void Swap(HPDataType* p1, HPDataType* p2)
+{
+	HPDataType tmp = *p1;
+	*p1 = *p2;
+	*p2 = tmp;
+}
+
+void AdjustUp(HPDataType* a, int child)
+{
+	int parent = (child - 1) / 2;
+	//while (parent >= 0)
+	while (child > 0)
+	{
+		if (a[child] > a[parent])
+		{
+			Swap(&a[child], &a[parent]);
+			child = parent;
+			parent = (child - 1) / 2;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+// 插入x继续保持堆形态
+void HeapPush(HP* php, HPDataType x)
+{
+	assert(php);
+	if (php->size == php->capacity)
+	{
+		int newCapacity = php->capacity == 0 ? 4 : php->capacity * 2;
+		HPDataType* tmp = (HPDataType*)realloc(php->a, newCapacity*sizeof(HPDataType));
+		if (tmp == NULL)
+		{
+			perror("realloc fail");
+			exit(-1);
+		}
+
+		php->a = tmp;
+		php->capacity = newCapacity;
+	}
+
+	php->a[php->size] = x;
+	php->size++;
+
+	AdjustUp(php->a, php->size - 1);
+}
+
+void AdjustDown(HPDataType* a, int n, int parent)
+{
+	int minChild = parent * 2 + 1;
+	while (minChild < n)
+	{
+		// 找出小的那个孩子
+		if (minChild+1 < n && a[minChild + 1] > a[minChild])
+		{
+			minChild++;
+		}
+
+		if (a[minChild] > a[parent])
+		{
+			Swap(&a[minChild], &a[parent]);
+			parent = minChild;
+			minChild = parent * 2 + 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+// 删除堆顶元素 -- 找次大或者次小
+// O(logN)
+void HeapPop(HP* php)
+{
+	assert(php);
+	assert(!HeapEmpty(php));
+	Swap(&php->a[0], &php->a[php->size - 1]);
+	php->size--;
+
+	AdjustDown(php->a, php->size, 0);
+}
+
+// 返回堆顶的元素
+HPDataType HeapTop(HP* php)
+{
+	assert(php);
+	assert(!HeapEmpty(php));
+
+	return php->a[0];
+}
+
+bool HeapEmpty(HP* php)
+{
+	assert(php);
+	return php->size == 0;
+}
+
+int HeapSize(HP* php);
